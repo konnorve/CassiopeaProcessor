9ec3cf6620803b5456d17b9a8892e7f77779c9da
lilianzhang392 <70563621+lilianzhang392@users.noreply.github.com>
2021-01-28 10:24:27 -0800

    Update FFMPEG_pt2.sh

--- shell_scripts/FFMPEG_pt2.sh
+++ shell_scripts/FFMPEG_pt2.sh
@@ -9,7 +9,7 @@
 #SBATCH --partition=savio2_htc
 #
 # Wall clock limit:
-#SBATCH --time=00:30:00
+#SBATCH --time=03:00:00
 #
 #SBATCH --mail-type=END,FAIL
 #
@@ -27,7 +27,7 @@ module load gnu-parallel/2019.03.22
 source activate myenv
 
 VIDEOSCRATCHPATH=/global/scratch/kve/Janis/20200726_Janis_606pm_cam1_1
-HOMEDIRPATH= $VIDEOSCRATCHPATH
+HOMEDIRPATH=$VIDEOSCRATCHPATH
 
 python3 /global/home/groups/fc_xenopus/utils/CassiopeaProcessor/FFMPEG_p2.py $VIDEOSCRATCHPATH $HOMEDIRPATH
 
