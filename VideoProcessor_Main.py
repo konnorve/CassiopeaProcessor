e8a60d4f3545886507bdff88fd3968781610b6eb
lilianzhang392 <70563621+lilianzhang392@users.noreply.github.com>
2021-09-13 16:08:52 -0700

    Updated to for overlays for image stacks

--- VideoProcessor_Main.py
+++ VideoProcessor_Main.py
@@ -6,14 +6,29 @@ import concurrent.futures
 import sys
 from time import sleep
 
-postInit_DF_path = Path(sys.argv[1])
+
+tmp_ImgStacks = Path(sys.argv[2]) # path to the /tmp/Image_Stacks directory within each overlay
+postInit_DF_path = Path(sys.argv[1]) # path to csv
 
 if __name__ == "__main__":
-    arg = int(sys.argv[2])
-    print(arg)
-    sleep(arg)
-    print(postInit_DF_path)
-    vpm.runFullVideoAnalysis(arg-1, postInit_DF_path)
+    
+    postInit_DF = pd.read_csv(postInit_DF_path)
+    
+    for file in tmp_ImgStacks.iterdir():
+        
+        if file.is_dir() and not (postInit_DF[postInit_DF['ChunkName'] == file.stem].empty):
+            
+            chunkName_idx = postInit_DF[postInit_DF['ChunkName'] == file.stem].index[0]
+
+            # arg = int(sys.argv[2]) # need to obtain arg as the index
+            arg = chunkName_idx
+            print(file.stem)
+            print(arg)
+            sleep(arg)
+            print(postInit_DF_path)
+            
+            vpm.runFullVideoAnalysis(arg, postInit_DF_path)
+
 
 
 # example running video processor in parallel on each chunk
